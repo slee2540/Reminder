8f9e78630ce5aae57a10ca543d2418514fb724b0
slee2540 <slee2540@gmail.com>
2020-03-15 01:03:51 +0900

    New / Component Task 생성

---
+++ src/components/Task/index.js
@@ -0,0 +1,21 @@
+import React from 'react';
+// import css from './sublist.module.css';
+// const data = ['1', '2', '3', '4', '5', '6'];
+// import {useSelector} from 'react-redux';
+
+const Task = () => {
+  // const subList = useSelector(state => state.list);
+
+  return (
+    <>
+      Task
+      {/* {subList.list.length > 0
+        ? subList.list.map(list => {
+            return <div>{list}</div>;
+          })
+        : null} */}
+    </>
+  );
+};
+
+export default Task;
