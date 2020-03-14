c0ce832671e0b516976f83331e97c0273622acb1
slee2540 <slee2540@gmail.com>
2020-03-15 01:07:56 +0900

    Update / 왼쪽부분 List 관련 화면 추가

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import SearchBox from 'components/SearchBox';
+import List from 'components/List';
+// import AddButton from 'components/AddButton';
+// import Task from 'components/Task';
+import css from './app.module.css';
 
 function App() {
-  return <div>메인화면</div>;
+  return (
+    <div className={css.wrapper}>
+      <div className={css.leftWrapper}>
+        <SearchBox />
+        <List />
+      </div>
+      <div className={css.rightWrapper}>Right</div>
+    </div>
+  );
 }
 
 export default App;
